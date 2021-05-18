4791810b75e56d34d30f16b15a5c16b6a21832e7
Simone Taeggi <startae14@gmail.com>
2021-05-18 23:49:37 +0200

    MAIN 03: define an action creator to fetch data

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 class PostList extends Component {
   render() {
     return (
@@ -10,4 +12,9 @@ class PostList extends Component {
   }
 }
 
-export default PostList;
+export default connect(
+  null,
+  {
+    fetchPosts
+  }
+)(PostList);
