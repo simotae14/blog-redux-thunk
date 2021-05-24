48d92456ca15987ae73026137fe559ecb8f182e1
Simone <startae14@gmail.com>
2021-05-24 23:49:27 +0200

    MAIN 04: try to run a request to an API with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+  // bad approach
+  const response = await jsonPlaceholder.get('/posts');
   return {
-    type: 'FETCH_POSTS'
+    type: 'FETCH_POSTS',
+    payload: response
   };
 };
