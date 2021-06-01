a1cc19dcb7e7cdb18b9603f4b226a45fc22bdf6f
Simone Taeggi <startae14@gmail.com>
2021-06-01 14:47:59 +0200

    MAIN 08: use mutation state operations in a reducer, wrong behaviour

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,4 +1,13 @@
 /* eslint-disable import/no-anonymous-default-export */
-export default () => {
-  return null;
+export default (state, action) => {
+  // bad!
+  state[0] = 'Sam';
+  state.pop();
+  state.push();
+
+  // bad!
+  state.name = 'Same';
+  state.age = 30;
+  
+  return state;
 };
