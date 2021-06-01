1fbd4a7b6752e81dc36d4b4009db70f2d57a3ead
Simone Taeggi <startae14@gmail.com>
2021-06-01 14:52:50 +0200

    MAIN 09: define a reducer body with the correct form using the switch operator

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,13 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
-export default (state, action) => {
-  // bad!
-  state[0] = 'Sam';
-  state.pop();
-  state.push();
-
-  // bad!
-  state.name = 'Same';
-  state.age = 30;
-  
-  return state;
+export default (state = [], action) => {
+  switch(action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default:
+      return state;
+  }
 };
