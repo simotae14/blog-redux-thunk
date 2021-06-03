ceba055cf10e79000dec942a4a81accf97c0b0b7
Simone Taeggi <startae14@gmail.com>
2021-06-03 23:20:05 +0200

    MAIN 17: memoizing an action with lodash memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,20 +9,12 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-// export const fetchUser = (id) => async dispatch => {
-//   const response = await jsonPlaceholder.get(`/users/${id}`);
-//   dispatch({
-//     type: 'FETCH_USER',
-//     payload: response.data
-//   });
-// };
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 
-export const fetchUser= function (id) { 
-  return _.memoize(async function (dispatch) {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-    dispatch({
-      type: 'FETCH_USER',
-      payload: response.data
-    });
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: 'FETCH_USER',
+    payload: response.data
   });
-};
+});
